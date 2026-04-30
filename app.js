fa0f2d5d24b39462770fd87bc2c52d3cf6f07706
xaviermanda <mandaxcosta@gmail.com>
2026-04-30 07:27:00 -0300

    Ajustando porta e start script

--- app.js
+++ app.js
@@ -3,30 +3,37 @@ const path = require("path");
 
 const app = express();
 
-// EJS
+// Configuração do EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// FORM
-app.use(express.urlencoded({ extended: true }));
+// Arquivos Estáticos (CSS, Imagens, JS do navegador)
+app.use(express.static(path.join(__dirname, "public")));
 
-// ROTAS
+// Middlewares para formulários e JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Adicione esta linha por segurança
+
+// Importação das ROTAS
 const categoriaRoutes = require("./routes/categoriaRoutes");
 const clienteRoutes = require("./routes/clienteRoutes");
 const fornecedorRoutes = require("./routes/fornecedorRoutes");
 
+// Uso das ROTAS
 app.use("/categorias", categoriaRoutes);
 app.use("/clientes", clienteRoutes);
 app.use("/fornecedores", fornecedorRoutes);
 
-// HOME
+// Rota HOME
 app.get("/", (req, res) => {
   res.render("index", {
     titulo: "Página Inicial",
   });
 });
 
-// SERVER
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
+// SERVER (Pronto para o Render e Local)
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
